ba871995aedc2aad289b804c982e255646c1bccb
Naturellement <pedigiodossouyovo@gmail.com>
2024-04-29 16:43:52 +0100

    Pushing Repo

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Groupomania - Accueil</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            text-align: center;
+            padding-top: 100px;
+        }
+
+        h1 {
+            color: #4e5166;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        p {
+            color: #4e5166;
+            font-size: 18px;
+            margin-bottom: 40px;
+        }
+
+        .btn-primary {
+            background-color:  #fd2d01;
+            border-color: #4e5166;
+            padding: 10px 40px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .btn-primary:hover {
+            background-color: #3c3e4d;
+            border-color: #3c3e4d;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <img src="Logos/icon-left-font-monochrome-black.svg" alt="">
+        <!-- <h1>Bienvenue sur Groupomania</h1> -->
+        <p>Rejoins nous .... On n'attends que toi</p>
+        <a href="login.php" class="btn btn-primary">Commencer</a>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
